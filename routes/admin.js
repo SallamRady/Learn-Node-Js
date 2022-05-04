188688ada34c1ab28de2d116cf3ba7e214791be8
SallamRady <sallamrady@gmail.com>
2022-05-04 21:00:38 +0200

    admin routes file

---
+++ routes/admin.js
@@ -0,0 +1,10 @@
+// Declaration...
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controllers/admin')
+// Routes..
+router.get('/',adminController.Dashboard);
+router.get('/addProduct',adminController.AddProduct);
+router.get('/product/:id',adminController.SingleProduct);
+
+module.exports = router
