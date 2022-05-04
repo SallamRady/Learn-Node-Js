f611d5fe1a0e67a46e1e540c1a3af0b732ac849c
SallamRady <sallamrady@gmail.com>
2022-05-04 20:59:52 +0200

    admin controller

---
+++ controllers/admin.js
@@ -0,0 +1,16 @@
+
+module.exports.Dashboard = (req,res,next)=>{
+    res.send('<h1>Dashboard Home Page</h1>');
+}
+
+module.exports.AddProduct = (req,res,next)=>{
+    res.send('<h1>AddProduct Page</h1>');
+}
+
+module.exports.CreateProduct = (req,res,next)=>{
+    res.send('<h1>CreateProduct Page</h1>');
+}
+
+module.exports.SingleProduct = (req,res,next)=>{
+    res.send('<h1>SingleProduct Page</h1>');
+}
